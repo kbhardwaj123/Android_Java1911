9f481d446c6347ad09dcb73367692457620d850a
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-19 15:21:59 +0530

    Addition.java: activity Depricated

--- app/src/main/java/com/example/myapplication/Addition.java
+++ app/src/main/java/com/example/myapplication/Addition.java
@@ -14,47 +14,47 @@ import android.widget.Toast;
 
 public class Addition extends AppCompatActivity {
 
-    EditText et1,et2;
-    Button b;
-    TextView ans;
+    // EditText et1,et2;
+    // Button b;
+    // TextView ans;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addition);
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //     super.onCreate(savedInstanceState);
+    //     setContentView(R.layout.activity_addition);
 
-        EditText etName,etEmail,etPass;
-        RadioButton r1,r2;
-        RadioGroup rg;
-        etEmail =(EditText) findViewById(R.id.input_email);
-        etName =(EditText) findViewById(R.id.input_name);
-        etPass =(EditText) findViewById(R.id.input_password);
-        r1 =(RadioButton) findViewById(R.id.f);
-        r2 =(RadioButton) findViewById(R.id.m);
-        rg =(RadioGroup) findViewById(R.id.radioGroup);
+    //     EditText etName,etEmail,etPass;
+    //     RadioButton r1,r2;
+    //     RadioGroup rg;
+    //     etEmail =(EditText) findViewById(R.id.input_email);
+    //     etName =(EditText) findViewById(R.id.input_name);
+    //     etPass =(EditText) findViewById(R.id.input_password);
+    //     r1 =(RadioButton) findViewById(R.id.f);
+    //     r2 =(RadioButton) findViewById(R.id.m);
+    //     rg =(RadioGroup) findViewById(R.id.radioGroup);
 
 
 
-        b = (Button) findViewById(R.id.btn_signup);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String name = etName.getText().toString();
-                final String email = etEmail.getText().toString();
-                final String pass = etPass.getText().toString();
-                String gender = "";
-                if(r1.isChecked()) gender ="Female";
-                if(r2.isChecked()) gender= "Male";
-                Toast.makeText(Addition.this, "Clicked",Toast.LENGTH_LONG).show();
+    //     b = (Button) findViewById(R.id.btn_signup);
+    //     b.setOnClickListener(new View.OnClickListener() {
+    //         @Override
+    //         public void onClick(View v) {
+    //             final String name = etName.getText().toString();
+    //             final String email = etEmail.getText().toString();
+    //             final String pass = etPass.getText().toString();
+    //             String gender = "";
+    //             if(r1.isChecked()) gender ="Female";
+    //             if(r2.isChecked()) gender= "Male";
+    //             Toast.makeText(Addition.this, "Clicked",Toast.LENGTH_LONG).show();
 
-                Intent intent = new Intent(Addition.this ,ShowData.class);
-                intent.putExtra("name", name);
-                intent.putExtra("email", email);
-                intent.putExtra("Gender", gender);
+    //             Intent intent = new Intent(Addition.this ,ShowData.class);
+    //             intent.putExtra("name", name);
+    //             intent.putExtra("email", email);
+    //             intent.putExtra("Gender", gender);
 
-                startActivity(intent);
-            }
-        });
+    //             startActivity(intent);
+    //         }
+    //     });
 
         //et1 = (EditText)findViewById(R.id.et1);
         //et2 = (EditText)findViewById(R.id.et2);
