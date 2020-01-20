d567679aa9687a9be5c3c28c7a5c23fca39911e7
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-20 18:16:24 +0530

    Notification: displays body of the basic notif sent via intent

--- app/src/main/java/com/example/myapplication/NotificationActivity.java
+++ app/src/main/java/com/example/myapplication/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        TextView tv = findViewById(R.id.text_view);
+
+        String message = getIntent().getStringExtra("message");
+        tv.setText(message);
     }
 }
